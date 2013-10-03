17ebce65c509ababfefd4f8cc3c4fdb158e2d3f4
Ezcaflowne <ezcaflowne@fullsail.edu>
2013-10-02 20:53:34 -0700

    added Linear Layout to view

--- week1/src/com/ayottewillson/week1/MainActivity.java
+++ week1/src/com/ayottewillson/week1/MainActivity.java
@@ -2,22 +2,29 @@ package com.ayottewillson.week1;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.widget.LinearLayout;
+
+
 
 public class MainActivity extends Activity {
+
+	// setup Variables
+	LinearLayout ll;
+	LinearLayout.LayoutParams lp;
+	
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        	
+        // initiate new linear layout
+        ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        
+        // initiate layout params
+        lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        ll.setLayoutParams(lp);
+        
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
 }
