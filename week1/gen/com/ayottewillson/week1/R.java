3f638f8d5d06fa6b70c399b2541d9fba0038fe18
Ezcaflowne <ezcaflowne@fullsail.edu>
2013-10-02 21:53:30 -0700

    finaled project, and added a settings menu.

--- week1/gen/com/ayottewillson/week1/R.java
+++ week1/gen/com/ayottewillson/week1/R.java
@@ -24,13 +24,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090000;
+    }
+    public static final class integer {
+        public static final int dataPrice=0x7f070001;
+        public static final int numPlans=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
