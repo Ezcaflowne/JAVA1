8e65126d660c0625a3fbe27487c3cb007be7142d
Ezcaflowne <ezcaflowne@fullsail.edu>
2013-10-07 23:03:07 -0700

    added forum class

--- week2/src/com/ayottewillson/week2/MainActivity.java
+++ week2/src/com/ayottewillson/week2/MainActivity.java
@@ -1,23 +1,58 @@
 package com.ayottewillson.week2;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.content.Context;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity {
+	public static LinearLayout singleEntryWithButton(Context context, String hint, String buttonText) {
+		// Create linear layout and parameters
+		LinearLayout lLayout = new LinearLayout(context);
+		LayoutParams lParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+		lLayout.setLayoutParams(lParams);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+		// Create edit text
+		EditText editText = new EditText(context);
+		// Set parameters for edit text to take up full width
+		lParams = new LayoutParams(0, LayoutParams.WRAP_CONTENT, 1.0f);
+		editText.setHint(hint);
+		editText.setLayoutParams(lParams);
+		editText.setId(1);
 
+		// Create button
+		Button getButton = new Button(context);
+		getButton.setText(buttonText);
+		getButton.setId(2);
+		// Tag button with edit text object
+		getButton.setTag(editText);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+		// Add UI elements to linear layout
+		lLayout.addView(editText);
+		lLayout.addView(getButton);
+
+		return lLayout;
+	}
+	public static  RadioGroup radioGroupOptions(Context context, String[] location) {
+		RadioGroup rGroup = new RadioGroup(context);
+
+		for (int i = 0; i < location.length; i++) {
+			RadioButton rButton = new RadioButton(context);
+			rButton.setText(location[i]);
+			rButton.setId(i+1);
+			rGroup.addView(rButton);
+		}
+
+		return rGroup;
+	}
+	public static TextView showResults(Context context) {
+		TextView textView = new TextView(context);
+		textView.setId(3);
+
+		return textView;
+	}
 }
